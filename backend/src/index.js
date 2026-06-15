a9a0e312bfd75c1da2cc4be6e86f27585000dc5f
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-06-15 17:02:37 +0100

    Add controllers for signup, login, and logout. Create middleware to protect routes from unauthorized access

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+
+import cookieParser from "cookie-parser";
+
 import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 const port = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
