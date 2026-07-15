fb632e141a06de5c141c596e898145c567848267
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-07-15 19:10:39 +0100

    Update ChatContainer component with MessageSkeleton and ChatHeader components

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 const ChatContainer = () => {
   const { selectedUser, messages, getMessages, isMessagesLoading } =
@@ -10,7 +11,15 @@ const ChatContainer = () => {
     getMessages(selectedUser._id);
   }, [selectedUser, getMessages]);
 
-  if (isMessagesLoading) return <div>Loading</div>;
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
