81adee24effc996b283696ae5e0e16a85c017c7b
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-07-16 16:58:04 +0100

    Upgrade backend with socket.io to detect connections/disconnections

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,10 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
+import { app, server, io } from "./lib/socket.js";
 dotenv.config();
 
-const app = express();
+app;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -22,7 +23,7 @@ const port = process.env.PORT;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   connectDB();
 });
