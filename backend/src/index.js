ab04b985f6deadb45c917c82b4d50bd4b781d09b
Emmanuel Paul <paulemmanuel8888@gmail.com>
2026-07-17 12:17:56 +0100

    Configure frontend and backend to build and run in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import cookieParser from "cookie-parser";
 
@@ -10,7 +11,7 @@ import { connectDB } from "./lib/db.js";
 import { app, server, io } from "./lib/socket.js";
 dotenv.config();
 
-app;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -22,6 +23,13 @@ const port = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
